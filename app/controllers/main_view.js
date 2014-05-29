d776cb54c8dfe1ff9338dae5abd23b668593605b
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2014-05-29 19:26:06 +0200

    Clean main_view controller

--- app/controllers/main_view.js
+++ app/controllers/main_view.js
@@ -1,13 +1,3 @@
-var main_scroll_view = $.main_scroll_view,
-	botoncito = Ti.UI.createButton({
-		title: 'Botoncito 2'
-	});
-
-main_scroll_view.add(botoncito);
-
-var drivers = Alloy.createCollection('drivers');
-Ti.API.info("Ahi van los drivers");
-Ti.API.info(drivers);
 
 var args = arguments[0] || {};
 
